a8c26153a06b63a2bc730002bf3cd32ec1d4a591
Jitendra <Jitendralodhidev@gmail.com>
2024-07-04 14:24:36 +0530

    Initial commit

---
+++ src/chat/chat.gateway.ts
@@ -0,0 +1,35 @@
+import {
+  WebSocketGateway,
+  WebSocketServer,
+  SubscribeMessage,
+  OnGatewayInit,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+} from '@nestjs/websockets';
+import { Logger } from '@nestjs/common';
+import { Socket, Server } from 'socket.io';
+
+@WebSocketGateway()
+export class ChatGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  @WebSocketServer() server: Server;
+  private logger: Logger = new Logger('ChatGateway');
+
+  @SubscribeMessage('message')
+  handleMessage(client: Socket, payload: { sender: string; message: string }): void {
+    this.server.emit('message', payload);
+  }
+
+  afterInit(server: Server) {
+    this.logger.log('Init');
+  }
+
+  handleDisconnect(client: Socket) {
+    this.logger.log(`Client disconnected: ${client.id}`);
+  }
+
+  handleConnection(client: Socket, ...args: any[]) {
+    this.logger.log(`Client connected: ${client.id}`);
+  }
+}
